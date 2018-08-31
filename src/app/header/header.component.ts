cb231a0be2c3c254266190b7c95c2b388455d520
Sergey Solonar <sergey.solonar@velocloud.net>
2018-08-31 20:11:56 +0300

    added icons refactored code

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import transcribe from 'rune-converter';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  nordicHeader = transcribe('Nordic');
 
   constructor() { }
 
